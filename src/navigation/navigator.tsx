2f9744109cfa9cff82f1866e5bc0387984a82e14
Miguel Cobian <miguelcobian99@gmail.com>
2023-08-17 12:12:58 -0600

    Feat. Detecting wich screen show depending os PermissionStatus

--- src/navigation/navigator.tsx
+++ src/navigation/navigator.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import MapScreen from '../screens/MapScreen';
 import PermissionsScreen from '../screens/PermissionsScreen';
+import { PermissionsContext } from '../context/PermissionsContext';
+import LoadingScreen from '../screens/LoadingScreen';
 
 const Stack = createStackNavigator();
 
 const Navigator = () => {
+    const { permissions } = useContext(PermissionsContext);
+
+    if (permissions.locationStatus === 'unavailable') {
+        return <LoadingScreen />;
+    }
+
     return (
         <Stack.Navigator
             screenOptions={{
@@ -16,8 +24,11 @@ const Navigator = () => {
                 },
             }}
         >
-            <Stack.Screen name="Permissions" component={PermissionsScreen} />
-            <Stack.Screen name="Map" component={MapScreen} />
+            {
+                (permissions.locationStatus === 'granted')
+                    ? <Stack.Screen name="Map" component={MapScreen} />
+                    : <Stack.Screen name="Permissions" component={PermissionsScreen} />
+            }
         </Stack.Navigator>
     );
 };
