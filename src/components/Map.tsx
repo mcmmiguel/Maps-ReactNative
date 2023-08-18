18a67a6c34f77856fa3e6b42428339cf48e4de69
Miguel Cobian <miguelcobian99@gmail.com>
2023-08-17 18:24:51 -0600

    Feat. Show User Location

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
-
-
 const Map = () => {
     return (
         <>
             <MapView
                 style={{ flex: 1 }}
+                showsUserLocation
                 provider={PROVIDER_GOOGLE}
                 initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
+                    latitude: 18.935640112827077,
+                    longitude: -103.96473259001439,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
             >
                 <Marker
                     coordinate={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
+                        latitude: 18.935640112827077,
+                        longitude: -103.96473259001439,
                     }}
                     title="AimeP3"
                     description="Ubicación de tu nutricionista"
--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,21 +1,12 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import Map from '../components/Map';
 
 const MapScreen = () => {
     return (
         <View style={{ width: '90%', height: '90%' }}>
             <Text>MapScreen</Text>
-            <MapView
-                style={{ flex: 1 }}
-                provider={PROVIDER_GOOGLE}
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            />
+            <Map />
         </View>
     );
 };
